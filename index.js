376adcb9089843bac8df6765ccd41a143ac5ef67
AbrarSian <abrarsian96@gmail.com>
2025-08-30 16:35:49 +0600

    modification of index js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,js}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        inter: ['Inter', 'sans-serif'],
+content: [
+    "./src/**/*.{html,js}", 
+],
+theme: {
+extend: {
+fontFamily: {
+        inter: ['Inter', 'sans-serif'], 
       },
     },
   },
-  plugins: [],
 }
